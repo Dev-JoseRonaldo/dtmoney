ed6bff3c94f1480bb46609fe454987bff983fb18
José Ronaldo <dev.joseronaldo@gmail.com>
2022-07-13 22:24:59 -0300

    :sparkles: feat: Configurando uma fake api com o miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import { Container } from "./style";
 
 export function TransactionsTable(){
+
+  useEffect(() =>{
+    fetch('http://localhost:3000/api/transactions')
+    .then(res => res.json())
+    .then( data => console.log(data))
+
+  }, [])
+
   return(
     <Container>
       <table>
