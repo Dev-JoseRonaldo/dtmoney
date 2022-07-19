b85911ea82e76a182ba762690eafc5d056da143f
José Ronaldo <dev.joseronaldo@gmail.com>
2022-07-18 23:55:56 -0300

    :sparkles: feat: Funcionamento dos botões

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished'
+import { darken } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -54,32 +54,38 @@ export const TransactionTypeContainer = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
+`
 
-    background: transparent;
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    transition: border-color 0.2s;
-    &:hover{
-      border-color: ${darken(0.2, '#d7d7d7')};
-    }
+  background: ${(props) => props.isActive ? '#eee' : 'transparent'};
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.2, '#d7d7d7')};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `
