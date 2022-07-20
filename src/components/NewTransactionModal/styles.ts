4fe41f5ec28fba555ab5dfded354229415d74714
José Ronaldo <dev.joseronaldo@gmail.com>
2022-07-19 21:25:48 -0300

    :sparkles: feat: Cores dos botões mudando ao selecionar

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken , transparentize } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -58,6 +58,13 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+
+const colors = {
+  green: '#33cc95',
+  red: '#e52e40'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -65,7 +72,9 @@ export const RadioBox = styled.button<RadioBoxProps>`
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive ? '#eee' : 'transparent'};
+  background: ${(props) => props.isActive
+   ? transparentize(0.8,colors[props.activeColor] )
+   : 'transparent'};
 
   display: flex;
   align-items: center;
