d6ced0c31c908e0fb1ec0421b1eba93ba8ef60ad
José Ronaldo <dev.joseronaldo@gmail.com>
2022-08-02 18:53:54 -0300

    :sparkles: feat: Inserindo transação na API e listando transações e seeds

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,12 @@
 import Modal from "react-modal";
+import { FormEvent, useState } from "react";
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 
+import { api } from "../../services/api";
+
 import { Container, TransactionTypeContainer, RadioBox} from "./styles";
-import { FormEvent, useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -22,12 +24,15 @@ export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionMod
   function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault()
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type
-    })
+    };
+
+
+    api.post('/transactions', data);
   }
 
   return(
